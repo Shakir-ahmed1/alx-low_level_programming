68d223a8956bf788419782c025c559dbcced14f7
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-09 14:39:38 -0800

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+		putchar(10);
+	}
+	return (0);
+}
