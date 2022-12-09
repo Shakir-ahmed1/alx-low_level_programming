ea749ec3bc62dc6ffd96c58ea239043fffe577bb
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-09 14:16:20 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c=0;
+
+	for(c=0;c<26;c++)
+	{
+		if ( c+97 != 101 && c+97 != 113)
+			putchar(c+97);
+		else
+			continue;
+	}
+	return (0);
+}
