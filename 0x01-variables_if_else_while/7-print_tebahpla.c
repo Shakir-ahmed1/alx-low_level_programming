84e443b6f944cf509b6e66051a0b52b2e94ebc69
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-09 14:46:41 -0800

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 122; num > 96; num++)
+	{
+		putchar(num);
+	}
+	putchar(10);
