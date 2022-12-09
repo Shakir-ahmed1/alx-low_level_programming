419952f53f995e549542786e6abeb23ed8ce3b0f
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-09 14:58:11 -0800

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <58; num++)
+	{
+		putchar(num);
+	}
+	for (num = 97; num < 97 + 6; num++)
+	{
+		putchar(num);
+	}
+	putchar(10)
+	return (0);
+}
