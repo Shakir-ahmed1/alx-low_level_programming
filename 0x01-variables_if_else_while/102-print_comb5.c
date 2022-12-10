1030a2603a8ca29427963288ee73f0d91f9a9154
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-09 16:20:01 -0800

    print comb

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+/**
+ * main - prints all combinations of two two-digit numbers
+ *
+ * Return: 
+ */
+int main(void)
+{
+	int num1, num2, num3, num4;
+
+	for (num1 = 48; num1 < 58; num1++)
+	{
+		for (num2 = 48; num2 < 58; num2++)
+		{
+			for (num3 = 48; num3 < 58; num3++)
+			{
+				for (num4 = 48; num4 < 58; num4++)
+				{
+					if (num1 + num2 < num3 + num4)
+					{
+						putchar(num1);
+						putchar(num2);
+						putchar(' ');
+						putchar(num3);
+						putchar(num4);
+						if (!(num1 + num2 == 56 + 57 && num3 + num4 == 57 + 57))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
