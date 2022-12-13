61f471e4dea5f4921ec0793a6eafb40ad4728d24
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-13 15:03:04 -0800

    functions and nested loop - advanced

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,30 +6,27 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-	
+
 	if (n > 15 || n < 0)
-	{
 		return;
-	}
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			k = i * j;
-			if (k < 10)
+			if (i * j < 10)
 			{
-				_putchar('0' + k);
+				_putchar('0' + i * j);
 			}
 			else if (k < 100)
 			{
-				_putchar('0' + k / 10);
-				_putchar('0' + k % 10);
+				_putchar('0' + (i * j) / 10);
+				_putchar('0' + (i * j) % 10);
 			}
 			else if (k <1000)
 			{
-				_putchar('0' + k / 100);
-				_putchar('0' + (k / 10) % 10);
-				_putchar('0' + k % 10);
+				_putchar('0' + (i * j) / 100);
+				_putchar('0' + ((i * j) / 10) % 10);
+				_putchar('0' + (i * j) % 10);
 			}
 			if (j != n)
 			{
