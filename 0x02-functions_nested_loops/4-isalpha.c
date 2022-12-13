b3c486c3c562de60b51a48d6a32e4d2211e46062
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-13 11:14:47 -0800

    islower

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _isalpha = checks if the given character is an alphabet
+ * _isalpha - checks if the given character is an alphabet
  * @c: the character to be checked
  * Return: 1 if it is an alphabet return 0 if it is not
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c < 97 + 26) && (c >= 65 || c < 65 + 26))
+	if ((c >= 97 && c < 97 + 26) || (c >= 65 && c < 65 + 26))
 	{
 		return (1);
 	}
