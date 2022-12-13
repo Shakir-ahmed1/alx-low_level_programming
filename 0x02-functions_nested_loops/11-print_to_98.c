47f98fb37acdc79db00115a0b59451e2ed82886d
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-13 13:55:21 -0800

    functions and nested loop

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_to_98 - it prints numbers between the given number and 98
+ * @n: starting number
+ * Return: Always 0
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		print("%d, ", i);
+	}
