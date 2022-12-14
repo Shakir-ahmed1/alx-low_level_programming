bebb72a318e3a3783ee3a4908b876d5f833430c1
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-13 16:30:12 -0800

    functions and nested loop - advanced

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - prints fabonacci series
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int i, l, k = 1, j = 1;
+
+	for (i = 1; i <= 97; i++)
+	{
+		l = j;
+		printf("%ld, ", j);
+		j = j + k;
+		k = l;
+	}
+	printf("%ld\n", j);
+	return (0);
+}
