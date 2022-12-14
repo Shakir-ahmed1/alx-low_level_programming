37ad8d777792a172663b64f31f6f5ddfcb6eab91
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-13 16:10:11 -0800

    functions and nested loop - advanced

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,13 +7,13 @@ int main(void)
 {
 	long int i, l, k = 1, j = 1;
 
-	for (i = 1; i <= 50; i++)
+	for (i = 1; i <= 49; i++)
 	{
 		l = j;
 		printf("%ld, ", j);
 		j = j + k;
 		k = l;
 	}
-	printf("\n");
+	printf("%ld\n",j);
 	return (0);
 }
