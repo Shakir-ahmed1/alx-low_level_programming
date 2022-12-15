c89c309c8b4bbe5cbae39d21ef74c6f5d080db2a
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-15 14:03:57 -0800

    mandatory

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,6 +15,12 @@ void print_remaining_days(int month, int day, int year)
 if (year % 4 == 0)
 {
 printf("Day of the year: %d\n", day);
+
+if (month >= 2 && day >= 60)
+{
+day++;
+}
+
 printf("Remaining days: %d\n", 366 - day);
 }
 else
