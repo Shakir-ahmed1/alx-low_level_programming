d7410e646a16a7759089df66043f711da8cf66cf
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-15 03:29:07 -0800

    mandatory

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks if a character is an uppers case
+ * @c: the character
+ * Return: 0 if not upper case 1 if upper case
+ */
+int _isupper(int c);
+{
+	if (c >= 65 && c <= 95)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
