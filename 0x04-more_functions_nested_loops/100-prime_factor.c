8d964f3dcbd3ac14f4800e0cbe011d561e098030
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-15 09:48:38 -0800

    advanced largest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -4,19 +4,9 @@
  * @num: the number to be checked
  * Return: 1 if the number is a prime 0 if the number is not a prime
  */
-int is_prime(long long int);
 int is_prime(long long int num)
 {
-long long int i;
-
-for (i = 2; i < num; i++)
-{
-if (num % i == 0)
-{
-return (0);
-}
-}
-return (1);
+	return (0);
 }
 
 /**
@@ -25,9 +15,22 @@ return (1);
  */
 int main(void)
 {
-long long int number;
+long long int i, number;
 
 number = 612852475143;
-is_prime(number);
+i = 2;
+
+while (is_prime(number) != 1)
+{
+if (number % i == 0 && is_prime(i))
+{
+number = number / i;
+i = 2;
+}
+
+i++;
+}
+
+printf("%lld\n", number);
 return (0);
 }
