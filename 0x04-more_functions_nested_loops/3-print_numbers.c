da81d9fdc30a9b2599052630ddf6237273de410a
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-15 04:03:21 -0800

    mandatory

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - it prints numbers from 0 to 9
+ * Return: always 0
+ */
+void print_numbers(void);
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	return (0);
+}
