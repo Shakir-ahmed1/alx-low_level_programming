eca73fcfb9cff03edfcabb6b2f91fa5515850864
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-15 04:18:24 -0800

    mandatory

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - it prints numbers from 0 to 9 except 4 and 2
+ * Return: always 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 || i != 52)
+		_putchar(i);
+	}
+	_putchar('\n');
+}
