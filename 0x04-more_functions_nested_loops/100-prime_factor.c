238a91aed2dde6600a1168006324c107ecd72493
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-15 07:06:55 -0800

    advanced

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+/**
+ * is_prime - checks if the given number is a prime
+ * @num: the number to be checked
+ * Return: 1 if the number is a prime 0 if the number is not a prime
+ */
+int is_prime(long long int num)
+{
+long long int i;
+
+for (i = 2; i < num; i++)
+{
+if (num % i == 0)
+{
+return (0);
+}
+}
+return (1);
+}
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+long long int i, number, p;
+
+number = 612852475143;
+i = 2;
+
+while (is_prime(number) != 1)
+{
+if (number % i == 0)
+{
+if (is_prime(i))
+{
+p = i;
+number = number / i;
+i = 2;
+}
+}
+i++;
+}
+printf("%lld\n", number);
+return (0);
+}
