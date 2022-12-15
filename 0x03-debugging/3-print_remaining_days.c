db998e116fc1cdb755e095dccefcce222762f29e
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-15 14:59:58 -0800

    mandatory

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,12 +14,12 @@ void print_remaining_days(int month, int day, int year)
 {
 if (year % 4 == 0)
 {
-
 if (month == 2 && day == 60)
 {
 day += 0;
 }
-else if (month > 2 && day > 60)
+
+if (month > 2 && day >= 60)
 {
 day++;
 }
