6645bb8cc7793b0719cd515241cbcda1b5fd72b1
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-15 04:41:46 -0800

    mandatory

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - it prints a line with n length
+ * @n: the length of the line
+ * Return: always 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_')
+	}
+	}
+	_putchar('\n');
+
+}
