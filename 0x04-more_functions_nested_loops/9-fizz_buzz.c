ce14addffc2b58bfb614dd15db796fd508cd597a
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-15 05:12:21 -0800

    mandatory

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+ * main - it prints fizz buzz
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+	if (i % 3 == 0)
+	{
+		printf("Fizz");
+	}
+	if (i % 5 == 0)
+	{
+		printf("Buzz");
+	}
+	if (i % 3 != 0 && i % 5 != 0)
+	{
+		printf("%d",i);
+	}
+	if (i != 100)
+	{
+		printf(" ");
+	}
+	}
+	printf("\n");
+}
