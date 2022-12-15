eda71d6712f997906c24004e7fd89569881d224d
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-15 11:10:27 -0800

    advanced printing numbers using putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,26 +5,16 @@
  */
 void print_number(int n)
 {
-int i, k, num, number;
+	unsigned int num = n;
 
-number = n;
-k = 1;
-if (number < 0)
-{
-	number = number * -1;
-	_putchar('-');
-}
-num = number;
-while (num > 9)
-{
-k *= 10;
-num /= 10;
-}
-for (i = 0; k != 1; i++)
-{
-_putchar('0' + (number / k));
-number = number % k;
-k /= 10;
-}
-_putchar('0' + number % 10);
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
