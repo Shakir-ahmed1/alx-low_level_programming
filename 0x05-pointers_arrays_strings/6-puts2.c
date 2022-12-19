f45b924a9aa625e937d040d27e662948b4ffd02c
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2022-12-19 14:08:12 -0800

    puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * puts2 - it prints half of the string
+ * @str: the string to be printed
+ */
+void puts2(char *str)
+{
+while (*str != '\0')
+{
+putchar(*str++);
+*str++;
+}
+}
