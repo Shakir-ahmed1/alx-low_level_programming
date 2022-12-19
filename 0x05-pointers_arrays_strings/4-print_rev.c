f8c19737aaaba9536630b96c10ccde3728a93759
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2022-12-19 11:28:04 -0800

    print in revers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,22 @@
 #include "main.h"
 /**
+ * _strlen - length of a string
+ * @s: the string
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+        int i = 0;
+
+        while (*s != '\0')
+        {
+                i++;
+                s++;
+        }
+        return (i);
+}
+
+/**
  * print_rev - it prints string in reverse
  * @s: the string
  */
