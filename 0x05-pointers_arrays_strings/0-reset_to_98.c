c79f8a8662b18ab24c15118d965c924c8500bcd1
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2022-12-19 07:51:05 -0800

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/*
+ * reset_to_98 - resets the given number to 98
+ * @n: the address of the number to be reseted
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,9 @@
+# Low-level programming - pointers, arrays, strings
+
+## Description
+
+it is a c programming exercise about pointers, arrays and strings
+
+# Structures
+
+
