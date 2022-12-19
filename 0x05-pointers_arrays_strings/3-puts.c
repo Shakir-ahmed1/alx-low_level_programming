09de86bec58fa01c002c7ed69b80acb9d6599961
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2022-12-19 11:07:19 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints characters
+ * @str: the stfffring to be printed
+ */
+void _puts(char *str)
+{
+int i = 0;
+
+while (*s != '\0')
+{
+
+_putchar(*s++);
+}
+}
+
