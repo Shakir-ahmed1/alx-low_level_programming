4d14bf84ceb1c6fd97a02d4febd15bcb2f3cbdc2
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2022-12-19 11:25:06 -0800

    print in revers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - it prints string in reverse
+ * @s: the string
+ */
+void print_rev(char *s)
+{
+	int size = _strlen(s);
+
+	while (size >= 0)
+	{
+		putchar(*(s + size));
+		size--;
+	}
+}
