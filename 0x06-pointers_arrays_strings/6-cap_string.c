c59ce04507c86680365052284b8de0ececa8b60f
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2022-12-21 23:08:12 +0000

    capitalize strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * cap_string - it capitalizes the given string
+ * @a: the string
+ * Return: a capitalized string
+ */
+char *cap_string(char *a)
+{
+	int i, j;
+	char b[] = " \n\t,;.!\?\"(){}";
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; b[j] != '\0'; j++)
+		{
+			if (a[i] == b[j])
+			{
+			if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+			{
+				a[i + 1] = a[i + 1] - 32;
+			}
+			}
+		}
+	}
+	return (a);
+}
