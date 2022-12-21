6226749207bd10b27fd969ab63e560c7ca74ee12
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2022-12-21 18:53:50 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strncpy - it copies a string
+ * @dest: the destination address
+ * @src: the source address
+ * @n: the limit of copying string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i, j = 0;
+
+
+for (i = 0; i < n; i++)
+{
+if (*(src + i) != '\0')
+{
+dest[i] = src[i];
+}
+else
+{
+j = 1;
+}
+if (j == 1)
+{
+*(dest + i) = '\0';
+}
+
+}
+return (dest);
+}
