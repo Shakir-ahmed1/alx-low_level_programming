445670d4e1c4aa28531d80e4b6ba00918503cd22
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2022-12-22 14:48:58 +0000

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,25 @@
 #include "main.h"
 /**
  * rot13 - it encodes the given string by using key = 13
- * @n: the address of string to be encoded
+ * @a: the address of string to be encoded
  * Return: the encoded string
  */
 char *rot13(char *a)
 {
-        int i;
+	int i;
 
-        for (i = 0; a[i] != '\0'; i++)
-        {
+	for (i = 0; a[i] != '\0'; i++)
+	{
 		int j;
 
-                if (a[i] >= 'a' && a[i] <= 'z')
-                {
-                        a[i] = (((a[i] - 'a')+ 13) % 26) + 'a';
-                }
-		for(j = 0; (a[i] >= 'A' && a[i] <= 'Z') && j == 0; j++)
-		{
-			a[i] = (((a[i] - 'A') + 13) % 26) + 'A';
-		}
+	if (a[i] >= 'a' && a[i] <= 'z')
+	{
+	a[i] = (((a[i] - 'a') + 13) % 26) + 'a';
+	}
+	for (j = 0; (a[i] >= 'A' && a[i] <= 'Z') && j == 0; j++)
+	{
+		a[i] = (((a[i] - 'A') + 13) % 26) + 'A';
+	}
 	}
 return (a);
 }
