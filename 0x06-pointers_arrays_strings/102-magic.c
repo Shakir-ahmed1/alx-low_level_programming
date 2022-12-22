4f40ce13207cec9fe93c1d752d8760658f0d32b3
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2022-12-22 16:11:55 +0000

    mag

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,8 +16,8 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  n = p - &a[2];
-  printf("%x",n);
+  *(&n + 5) = 98;
+  printf("%d\n",n);
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
