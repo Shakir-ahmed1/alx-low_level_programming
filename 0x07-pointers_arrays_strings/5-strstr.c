1301cb654efb3132c01d042ae7eee8eeb9727324
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-02 20:37:34 +0000

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,24 +7,24 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-int i,j;
+int i;
 
-for (i = 0; *(haystack + i) != '\0'; i++)
+if (*needle == 0)
+return (haystack);
+
+while (*haystack)
 {
-if (*(haystack + i) == *(needle))
+i = 0;
+
+if (haystack[i] == needle[i])
 {
-for ( j = 0; *(needle + j) != '\0'; j++)
-{
-if (*(needle + j) != *(haystack + i + j))
-{
-break;
+do {
+if (needle[i + 1] == '\0')
+return (haystack);
+i++;
+} while (haystack[i] == needle[i]);
 }
+haystack++;
 }
-}
-if (*(needle + j) == '\0')
-{
-return (haystack + i);
-}
-}
-return (0);
+return ('\0');
 }
