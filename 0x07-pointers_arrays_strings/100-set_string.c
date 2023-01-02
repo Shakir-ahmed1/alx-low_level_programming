6f520292b850bcea3b994b8c88216b5521cdbb45
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-02 21:52:08 +0000

    double pointer double fun

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * set_string - it copies a string to a double pointer
  * @s: the destination
@@ -7,11 +6,6 @@
  */
 void set_string(char **s, char *to)
 {
-	/*int i;
-	for (i = 0; *(to + i) != '\0'; i++)
-	{
-		*(*s + i) = *(to + i);
-	}*/
 	*s = to;
 	(void)*s;
 }
