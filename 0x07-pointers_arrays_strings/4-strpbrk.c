6a6b20c8696bfc8411067c3dde567335a33c483d
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-02 19:07:43 +0000

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the main string
+ * @accept: the substring to be indexed
+ * Retrun: the string including the first occurance of the substring
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(accept + j) == *(s + i))
+			{
+				break;
+			}
+		}
+		if (*(accept + j) != '\0')
+		{
+			return (s+i);
+		}
+	}
+
+	return (0);
+}
