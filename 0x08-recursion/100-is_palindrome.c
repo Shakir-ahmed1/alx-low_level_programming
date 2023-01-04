332abda3bf0b4f419cc484b1d1580ad5f2593971
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-04 20:08:40 +0000

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -16,24 +16,37 @@ int is_palindrome(char *s)
 	{
 		return (1);
 	}
-	return (_palindrome(s,0,len));
+	return (_palindrome(s, 0, len));
 }
+/**
+ * _strlen - it returns the length of the given string
+ * @s: the string
+ * @i: counter
+ * Return: the length of the string
+ */
 int _strlen(char *s, int i)
 {
-	if ( *(s + i) != '\0')
+	if (*(s + i) != '\0')
 	{
 		i++;
-		return _strlen(s,i);
+		return (_strlen(s, i));
 	}
-	return i;
+	return (i);
 }
+/**
+ * _palindrome - it checks if the string is palindrome
+ * @s: the string
+ * @n: indexer
+ * @len: the length of the string
+ * Return: 1 if palindrome, 0 if not
+ */
 int _palindrome(char *s, int n, int len)
 {
 	if (s[n] != s[(len - n - 1)] && s[n] != '\0')
 	{
 		return (0);
 	}
-	if (len/2 != n)
+	if (len / 2 != n)
 	{
 		return (_palindrome(s, n + 1, len));
 	}
