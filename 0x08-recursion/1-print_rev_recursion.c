569d4619d39bcd35ea834dc5a9fdb337cd980715
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-04 13:18:56 +0000

    print in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - it prints a string using _putchar using recursion
+ * @s: the string t be printed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(++s);
+		_putchar(*s);
+	}
+}
