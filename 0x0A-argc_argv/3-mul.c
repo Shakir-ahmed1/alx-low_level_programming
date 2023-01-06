b2446b11c606d02692de76b2f70eaacfa8127299
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-06 20:01:26 +0000

    args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - it multiplies two numbers
+ * @argc: the argument size
+ * @argv: the address to the first string
+ */
+int main(int argc,int **argv)
+{
+	if (argc == 3)
+	{
+		printf("%d", argv[1] * argv[2]);
+		return (0);
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+	return (0);
+}
