579ab33bb3001ed53c4b7fe7fe201a341a031b5d
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-06 20:12:42 +0000

    atoi

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - it multiplies two numbers
  * @argc: the argument size
  * @argv: the address to the first string
  */
-int main(int argc,int **argv)
+int main(int argc,char **argv)
 {
 	if (argc == 3)
 	{
-		printf("%d", *argv[1] * *argv[2]);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
 	else
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
 	return (0);
