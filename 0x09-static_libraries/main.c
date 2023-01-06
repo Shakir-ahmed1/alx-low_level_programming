ba66deb0346961b5cb506f590b00e773cbbdd74a
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-06 14:58:34 +0000

    archived

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,10 +1,7 @@
-#include<stdio.h>
 #include "main.h"
 
-int main()
+int main(void)
 {
-	int i;
-
-	(void)i;
-	return 0;
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
 }
