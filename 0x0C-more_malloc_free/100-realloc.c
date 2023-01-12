7d9da4a5d63eb4aa7e6b856e40b0b1b7792a5c4a
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-12 17:38:11 +0000

    submit

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * realloc - it realocates the memory of a pointer with the specified length
+ * @ptr: the pointer to be realocated
+ * @old_size: the size of the pointer
+ * @new_size: the new size of ptr
+ * Return: NULL, ptr, the new allocated pointer uninitialized and initialized
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *mc;
+	unsigned int i;
+	
+	if (ptr == NULL)
+	{
+		mc = malloc(new_size);
+		return (mc);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	mc = malloc(new_size * sizeof(int));
+	if (mc == NULL)
+		return (NULL);
+	if (new_size > old_size)
+		return (mc);
+	else
+	{
+		for (i = 0; i < new_size; i++)
+			*((char *) mc + i) = *((char *) ptr + i);
+		return (mc);
+	}
+}
+
