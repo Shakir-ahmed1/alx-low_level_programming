ebf68b0e8a06af353e2cfb53907d07b1b3b73f9e
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-12 17:53:20 +0000

    submit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,6 +15,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		mc = malloc(new_size);
+		if (mc == NULL)
+			return (NULL);
 		return (mc);
 	}
 	if (new_size == 0 && ptr != NULL)
@@ -25,6 +27,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	mc = malloc(new_size * sizeof(int));
+	if (mc == NULL)
+		return (NULL);
 	if (mc == NULL)
 		return (NULL);
 	if (new_size > old_size)
