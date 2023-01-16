281e1e1679c8dadc04f67e9eca7df5856dafcac0
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-16 18:59:04 +0000

    submit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,20 @@
 #include "dog.h"
 /**
+ * _strcpy - copies a string from an address to another
+ * @src - source string
+ * @dest - destination string
+ * Return: the destination address
+ */
+char *_strcpy(char *src,char  *dest)
+{
+	int i = 0;
+
+	for (;src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+/**
  * new_dog - it returns a new dog
  * @name: dog name
  * @age: dog age
@@ -12,12 +27,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	nd = malloc(sizeof(dog_t));
 	if (nd == NULL || name == NULL || owner == NULL)
-	{
 		return (NULL);
-	}
-	nd->name = name;
+
+	nd->name = malloc(sizeof(name));
+	if (nd->name == NULL)
+		return (NULL);
+	nd->name = _strcpy(name, nd->name);
+
 	nd->age = age;
-	nd->owner = owner;
+
+	nd->owner = malloc(sizeof(owner));
+	if (nd->owner == NULL)
+		return (NULL);
+	nd->owner = _strcpy(owner, nd->owner);
 
 	return (nd);
 }
