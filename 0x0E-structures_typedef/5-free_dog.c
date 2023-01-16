b803d8a48909a0234b61071f83a70d5b063eaf03
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-16 20:03:14 +0000

    submit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * free_dog - it frees memory allocated to a dog
+ * @d: the address of the dog to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			free(d->name);
+		if (d->owner != NULL)
+			free(d->owner);
+		free(d);
+	}
+	
+}
