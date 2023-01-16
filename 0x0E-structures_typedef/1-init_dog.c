968cdc4e4d27ee440c47a8167478d8d928e7f4e0
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-16 16:45:11 +0000

    submit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * init_dog - initializes struct dog
+ * @d: the addres of the dog to be intialized
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include "dog.h"
+void init_dog(struct dog *d, char *name, float age, char *owner);
+/* dog_t *new_dog(char *name, float age, char *owner); */
+#endif
