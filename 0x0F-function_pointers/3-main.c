03bb0b18c8e444228567894c7e1704e8263723f0
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-18 21:16:51 +0000

    submit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,26 +5,26 @@
  * @argv: the array of strings passed to the program
  * Return: always 0
  */
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
 	if (argc != 4)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(98);
 	}
 	if ((*argv[2] == '/' && atoi(argv[3]) == 0) ||
 		       (*argv[2] == '%' && atoi(argv[3]) == 0))
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(100);
 	}
 
 	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n",get_op_func(argv[2])(atoi(argv[1]),atoi(argv[3])));
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,21 +1,50 @@
 #include "3-calc.h"
-
+/**
+ * op_add - it adds two numbers
+ * @a: first number
+ * @b: the second number
+ * Return: always (a + b)
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+/**
+ * op_sub - it subtracts two numbers
+ * @a: first number
+ * @b: the second number
+ * Return: always (a - b)
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+/**
+ * op_mul - it multiplies two numbers
+ * @a: first number
+ * @b: the second number
+ * Return: always (a * b)
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * op_div - it devides two numbers
+ * @a: first number
+ * @b: the second number
+ * Return: always (a / b)
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+/**
+ * op_mod - it finds the modules of two numbers
+ * @a: first number
+ * @b: the second number
+ * Return: always (a % b)
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
