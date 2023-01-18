ecdcb202b6407f8c10a9e1941c9c2c452e009fe6
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-18 21:08:52 +0000

    submit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,7 +24,7 @@ int main(int argc,char **argv)
 		printf("Error");
 		exit(99);
 	}
-	printf("%d",get_op_func(argv[2])(atoi(argv[1]),atoi(argv[3])));
+	printf("%d\n",get_op_func(argv[2])(atoi(argv[1]),atoi(argv[3])));
 	return (0);
 
 }
