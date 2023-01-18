aaeddf746d3a44174ed536c9ae4d3275172dcca5
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-18 22:56:03 +0000

    submit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,39 +1,31 @@
 #include "3-calc.h"
-/**
- * get_op_func - it selects the correct function to do the operation
- * @s: the operator passed by the program
- * Return: This function returns a pointer to the function that
- * corresponds to the operator given as a parameter.
- * Example: get_op_func("+") should return a pointer to the function op_add
- * You are not allowed to use switch statements
- * You are not allowed to use for or do ... while loops
- * You are not allowed to use goto
- * You are not allowed to use else
- * You are not allowed to use more than one if statement in your code
- * You are not allowed to use more than one while loop in your code
- * If s does not match any of the 5 expected
- * operators (+, -, *, /, %), return NULL
- * You are only allowed to declare these two variables in this function:
- */
-int (*get_op_func(char *s))(int a, int b)
-{
-	op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
-	};
-	int i = 0;
+#include <stdlib.h>
 
-	while (i <  6)
-	{
-		if (*s == *(ops[i].op))
-		{
-			return (ops[i].f);
-		}
-		i++;
-	}
-	return (NULL);
+/**
+*get_op_func - selects the correct function to perform
+*the operation asked by the user. You’re not allowed
+*to declare any other function.
+*@s: operator passed as argument
+*
+*Return: A pointer to the function corresponding to
+*the operator given in parameter
+*/
+int (*get_op_func(char *s))(int, int)
+{
+
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+
+while (ops[i].op != NULL && *(ops[i].op) != *s)
+i++;
+
+return (ops[i].f);
+
 }
