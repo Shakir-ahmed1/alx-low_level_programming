22931e21c36efd1ac3a75908dfa62959abbb6f9a
Shakir1-Ahmed <shakirahmedsalih35756@gmail.com>
2023-01-19 22:21:56 +0000

    submit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", i);
-		if (separator)
+		if (separator && j != n - 1)
 			printf("%s", separator);
 		j++;
 	}
