1d6e983ce42b1a7d09727876a50b10dc928b16fa
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-01-30 15:41:34 +0000

    fix bug

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,7 +10,7 @@ size_t print_listint(const listint_t *h)
 	unsigned int count = 0;
 
 	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (new == NULL || h == NULL)
 		return (0);
 	temp = new;
 	new->n = h->n;
