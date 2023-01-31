50db015b76dd74f63d095bf6bf812e6f3d9fcb81
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-01-31 14:30:02 +0000

    submit advanced

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,14 +6,16 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-    listint_t *temp = NULL;
-    listint_t *prev = NULL;
-    while((*head) != NULL) {
-        temp = (*head)->next;
-        (*head)->next = prev;
-        prev = (*head);
-        (*head) = temp;
-    }
-    (*head) = prev;
-    return (*head);
+	listint_t *temp = NULL;
+	listint_t *prev = NULL;
+
+	while ((*head) != NULL)
+	{
+		temp = (*head)->next;
+		(*head)->next = prev;
+		prev = (*head);
+		(*head) = temp;
+		}
+	(*head) = prev;
+	return (*head);
 }
