3f034642ebcf9b7657acdb0bcb5d80a9be88f927
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-01-31 22:54:31 +0000

    submit advanced

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,48 +1,29 @@
 #include "lists.h"
+
 /**
- * print_listint_safe - it prints all elements of listint_t
- * @head: the first node
- * Return: number of nodes
+ * print_listint_safe - prints a linked list, safely
+ * @head: list of type listint_t to print
+ *
+ * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	listint_t *new, *temp, *new2, *temp2;
-	unsigned int count = 0, count2 = 0;
+	size_t num = 0;
+	long int diff;
 
-	new = malloc(sizeof(listint_t));
-	new2 = malloc(sizeof(listint_t));
-	if (new == NULL || head == NULL || new2 == NULL)
-		return (0);
-	temp = new, temp2 = new2;
-	new->n = head->n, new->next = head->next;
-	new2->n = new->n, new2->next = new->next;
-	if (new == new->next)
+	while (head)
 	{
-		printf("[%p] %d\n", (void *)new, new->n);
-		count++;
+		diff = head - head->next;
+		num++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
 	}
-	while (new != NULL && new != new->next)
-	{
-		count2 = 0;
 
-		new2 = temp2;
-		while (new->next !=  new2->next)
-		{
-			new2 = new2->next;
-			count2++; }
-		if (count != count2)
-		{
-			printf("[%p] %d\n", (void *)new, new->n);
-			count++;
-			new = new->next;
-			printf("-> [%p] %d\n", (void *)new, new->n);
-			head = new;
-			free(temp), free(temp2);
-			return (count); }
-		count++;
-		printf("[%p] %d\n", (void *)new, new->n);
-		new = new->next;
-	}
-	free(temp), free(temp2);
-	return (count);
+	return (num);
 }
