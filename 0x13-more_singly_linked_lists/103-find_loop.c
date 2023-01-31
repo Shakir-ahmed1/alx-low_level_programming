a342119d60a71d4dbf14bb87612f0fe87cef7ad3
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-01-31 22:34:55 +0000

    submit advanced

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -7,16 +7,18 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *temp = NULL;
-    listint_t *nex = NULL;
-    while (head != NULL) {
-        if (head->next == NULL)
-            return (NULL);
-        if (head->next == temp)
-            break;
-        nex = head->next;
-        head->next = temp;
-        head = nex;
-    }
-    return (head);
+	listint_t *temp = NULL;
+	listint_t *nex = NULL;
+
+	while (head != NULL)
+	{
+	if (head->next == NULL)
+		return (NULL);
+	if (head->next == temp)
+		break;
+	nex = head->next;
+	head->next = temp;
+	head = nex;
+	}
+	return (head);
 }
