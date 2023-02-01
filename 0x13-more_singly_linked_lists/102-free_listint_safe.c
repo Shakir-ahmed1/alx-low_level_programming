c28691926525fcc234208fca23023f4834e935fd
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-02-01 09:54:07 +0000

    bug fix

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,31 +8,21 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t len = 0;
-	int diff;
-	listint_t *temp;
+        listint_t *new;
+        unsigned int count = 0;
 
-	if (!h || !*h)
-		return (0);
-
-	while (*h)
-	{
-		diff = *h - (*h)->next;
-		if (diff > 0)
-		{
-			temp = (*h)->next;
-			*h = temp;
-			len++;
-		}
-		else
-		{
-			*h = NULL;
-			len++;
-			break;
-		}
-	}
-
-	*h = NULL;
-
-	return (len);
+        if (h == NULL || *h == NULL)
+                return (0);
+        while (*h != NULL)
+        {
+                new = (*h)->next;
+                (*h)->next = NULL;
+                free(*h);
+                *h = new;
+                count++;
+        }
+        if (count != 0)
+                count = count - 2;
+        h = NULL;
+        return (count);
 }
