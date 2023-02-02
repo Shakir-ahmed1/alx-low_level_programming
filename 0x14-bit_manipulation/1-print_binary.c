4f193b5c4254507962e2ac066978e6af6f142a88
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-02-02 14:31:36 +0000

    submit advanced

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary - it prints the given long integer as binary
+ * @n: the integer to be printed in binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int a = 1;
+	int i = 0;
+	while ((a << 1) <= n)
+	{
+		a = a << 1;
+		i++;
+	}
+	for (; i >= 0;i--)
+	{
+		if ((a & n) == 0)
+			_putchar('0');
+		else
+			_putchar('1');
+		a = a >> 1;
+	}
+
+
+
+}
+
