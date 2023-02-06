389e5c9efd9c299e64f39e6c2e49dc48f15bbea1
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-02-06 19:44:26 +0000

    adding task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,18 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * read_textfile - it reads a text file and prints it to the POSIX stdout
- * @filname: the name of the file to read from
+ * @filename: the name of the file to read from
  * @letters: the number of letters it should read and print
  * Return: the actual number of letters it could read and print
  * if file cannot opened or file name is NULL or if write fails return 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buff[500];
-	int fd, i;
+	char buff[5000];
+	int fd, i, j;
 	ssize_t length;
 	mode_t mode = O_RDONLY;
-	
+
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, mode);
@@ -21,9 +22,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	length = read(fd, buff, letters);
 	lseek(fd, SEEK_SET, 0);
 	close(fd);
+	j = 0;
 	for (i = 0; i < length; i++)
 	{
-		_putchar(buff[i]);
+		printf("%d\n", j);
+		j = _putchar(buff[i]);
+		if (j != 1)
+			return (0);
 	}
 	return (length);
 }
