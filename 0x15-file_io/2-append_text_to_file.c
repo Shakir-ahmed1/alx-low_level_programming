a2ed9d04a9f8577e392f4927eef0a809bb5c9d41
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-02-07 17:50:57 +0000

    solving task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,8 +19,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
+	if (fd != -1 && !filename)
+		return (1);
 	write(fd, text_content,len);
 
-	(void) text_content;
+	close(fd);
 	return (1);
 }
