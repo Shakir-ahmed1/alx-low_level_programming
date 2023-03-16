75b707258ae5c49a6e24346878c2f12e5ee80993
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-16 15:11:55 +0000

    insert code at the given index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp, *new;
-	unsigned int count = 5;
+	unsigned int count = 0;
 
 	if (h == NULL)
 		return (NULL);
