ef3066842747fc22d394640a07055676dae929cb
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-16 12:43:05 +0000

    bug fix

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	if (head == NULL)
 		return (NULL);
-	new = malloc(sizeof(dlistint_t *));
+	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
