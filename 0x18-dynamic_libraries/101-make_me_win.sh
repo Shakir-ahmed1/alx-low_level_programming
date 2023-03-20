b30a58b0dab62e068f862e1fd71b142ed98cb721
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-20 21:59:42 +0000

    submit code

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Pericles001/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD =/tmp/nrandom.so
+wget -P .. https://github.com/Pericles001/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD = "$PWD/../nrandom.so"
