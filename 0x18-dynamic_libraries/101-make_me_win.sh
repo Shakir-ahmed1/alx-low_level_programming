91c6fe7f360e0f41b66e4fb1d8a86707e481c9b0
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-20 22:47:58 +0000

    subit code

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/Pericles001/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD = "$PWD/../nrandom.so"
+wget -P .. https://github.com/Shakir-ahmed1/alx-low_level_programming/raw/master/0x18-dynamic_libraries/crack.so
+export LD_PRELOAD="$PWD/../crack.so"
