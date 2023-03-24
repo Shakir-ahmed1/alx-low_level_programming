a3f3f89c3e2a76ceeac17e5a8e11392533c49ff6
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-03-24 21:25:46 +0000

    bug fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P .. https://github.com/Shakir-ahmed1/alx-low_level_programming/raw/master/0x18-dynamic_libraries/shaklib.so
-LD_PRELOAD="$PWD/../shaklib.so" ./gm 9 8 10 24 75 9
+export LD_PRELOAD="$PWD/../shaklib.so"
