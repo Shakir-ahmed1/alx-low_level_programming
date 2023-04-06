7ee494ddf395fb2f80dee77f92c14f69a4823999
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-06 21:04:13 +0000

    first commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,15 +9,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new;
 	unsigned long int i;
 
-	if (size < 1)
-		return (NULL);
 	new = malloc(sizeof(hash_table_t));
 	if (!new)
 		return (NULL);
 	new->size = size;
 	new->array = calloc(size, sizeof(hash_node_t));
 	if (new->array == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 		new->array[i] = NULL;
 	return (new);
