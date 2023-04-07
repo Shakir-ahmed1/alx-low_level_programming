af60dc708ff44eb2f2d9a2c86982e204acb57b49
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-07 15:21:38 +0000

    fixing memory allocation

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - assignes a new element to the table
+ * @ht: the hash table
+ * @key: the key
+ * @value: the value to be stored
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned int index;
+	hash_node_t *new;
+
+	if (ht == NULL)
+		return (0);
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	new->key = malloc(strlen(key) + 1);
+	new->value = malloc(strlen(key) + 1);
+	if (new->key == NULL || new->value == NULL)
+		return (0);
+	strcpy(new->key, key);
+	strcpy(new->value, value);
+	new->next = NULL;
+	
+	index = key_index((const unsigned char *) key, ht->size);
+	ht->array[index] = new;
+	return (1);
+}
