fbdc56aa72c6438d1328942f28efccb912eb1074
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-07 17:09:36 +0000

    fixing memory allocation

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,6 +35,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	temp = new;
 */	(void) temp;
-	ht->array[index] = new;
+	if (temp == NULL)
+		ht->array[index] = new;
 	return (1);
 }
