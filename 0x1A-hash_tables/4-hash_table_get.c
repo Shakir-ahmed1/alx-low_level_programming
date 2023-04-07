b32255eed1868a2aa47bb24eb4ecc8467919594b
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-04-07 17:44:09 +0000

    submit code

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - gets a value with a given key
+ * @ht: the table
+ * @key: the key
+ * Return: the value or NULL if the key couldn't be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *temp;
+	unsigned long int index;
+
+	index = key_index((const unsigned char *) key, ht->size);
+
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
+}
