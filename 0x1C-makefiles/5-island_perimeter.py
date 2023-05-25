2db10cf112a12308e9fa7058bc2ea9c821731c88
Shakir1-ahmed <shakirahmedsalih35756@gmail.com>
2023-05-25 02:07:10 +0000

    Island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" a module that finds the perimeter of an island"""
+
+
+def island_perimeter(grid):
+    """ calculates the perimeter of a island """
+    prm = 0
+    ac = len(grid[0])
+    for a in range(len(grid)):
+        for b in range(ac):
+            if grid[a][b] == 1:
+                print("didid")
+                if grid[a-1][b] == 0:
+                    prm += 1
+                    print(grid[a-1][b])
+                if grid[a][b + 1] == 0:
+                    prm +=1
+                    print(grid[a][b+1])
+                if grid[a + 1][b] == 0:
+                    prm +=1
+                    print( grid[a+1][b])
+                if grid[a][b - 1] == 0:
+                    prm +=1
+                    print(grid[a][b-1])
+    return prm 
