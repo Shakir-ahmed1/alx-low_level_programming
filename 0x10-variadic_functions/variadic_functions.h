77f8ec10ec73580874dfdc9b373bc3c8d5b606fd
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-06-19 22:20:03 +0000

    fixing documentation and codding style

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,9 +8,16 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-typedef struct
+/**
+ * struct print_s - prints the given function
+ * @id: the id of the function
+ * @print: the function to be called
+ *
+ * Description: a struct for printing multiple arguments
+ */
+typedef struct print_s
 {
 	char id;
 	void (*print)(va_list a);
-}print_t;
+} print_t;
 #endif
