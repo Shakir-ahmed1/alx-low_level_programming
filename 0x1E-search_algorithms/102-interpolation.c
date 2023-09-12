c1d6c31e4b40e1488a02ba263cf2ccdeecb1b7e4
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-12 16:12:36 +0000

    interpolation

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+/**
+ * interpolation_search - jump searching algorithm implementation
+ * @array: the index to the first array
+ * @size: the size of the array
+ * @value: the value to be searched
+ * Return: the index of the value if found, else return -1
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t low, high, pos;
+
+	if (array == NULL)
+		return (-1);
+	low = 0;
+	high = size - 1;
+	while (low <= high)
+	{
+		pos = low + (((double)(high - low) /
+					(array[high] - array[low])) * (value - array[low]));
+		if (pos >= size)
+		{
+			printf("value checked array[%ld] is out of range", pos);
+			return (-1);
+		}
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+		if (array[pos] == value)
+			return (pos);
+		if (array[pos] > value)
+		{
+			high = pos - 1;
+		}
+		else if (array[pos] < value)
+		{
+			low = pos + 1;
+		}
+	}
+	return (-1);
+}
