ee557275a760e5fe20e7cbb49cce8acb4f5e2acc
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-13 19:44:29 +0000

    jump search using linked list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -4,7 +4,7 @@
 /**
  * jump_list - jump search using linked list
  *
- * @array: Pointer to the first element in the linked list
+ * @list: Pointer to the first element in the linked list
  * @size: Size of the array
  * @value: the value to be searched
  *
@@ -16,33 +16,33 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 	listint_t *temp;
 
 	temp = list;
-        if (list == NULL || size == 0)
-                return (NULL);
-        m = sqrt(size);
-        i = 0;
-        while (temp->n < value)
-        {
+	if (list == NULL || size == 0)
+		return (NULL);
+	m = sqrt(size);
+	i = 0;
+	while (temp->n < value)
+	{
 		if (i + m >= size - 1)
 			i = size - 1;
 		else
-                	i += m;
-                if (temp->next == NULL)
-                        break;
+			i += m;
+		if (temp->next == NULL)
+			break;
 		for (k = 0; k < m && temp->next != NULL; k++)
 			temp = temp->next;
-                printf("Value checked array[%ld] = [%d]\n", i, temp->n);
-        }
-        printf("Value found between indexes [%ld] and [%ld]\n", i - k, i);
+		printf("Value checked array[%ld] = [%d]\n", i, temp->n);
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n", i - k, i);
 	temp = list;
 	for (n = 0; n < i - k; n++)
 		temp = temp->next;
-        for (j = i - k; j < i + 1; j++)
-        {
-                printf("Value checked array[%ld] = [%d]\n", j, temp->n);
-                if (temp->n == value)
-                        return (temp);
+	for (j = i - k; j < i + 1; j++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", j, temp->n);
+		if (temp->n == value)
+			return (temp);
 		temp = temp->next;
-        }
-        return (NULL);
+	}
+	return (NULL);
 }
 
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,27 @@
 #ifndef _SEARCH_ALGOS_H_
 #define _SEARCH_ALGOS_H_
 #include <stdio.h>
+/* data structures */
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+/* helper functions */
+listint_t *create_list(int *array, size_t size);
+void free_list(listint_t *list);
+void print_list(const listint_t *list);
 
 /* prototypes */
 int linear_search(int *array, size_t size, int value);
@@ -9,4 +30,5 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 #endif
