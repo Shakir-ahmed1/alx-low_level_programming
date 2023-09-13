fb68c4396be4f7f807734ef3adeaead27cbee8e9
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-13 19:57:08 +0000

    jump search using linked list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -12,7 +12,7 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t m, i, j, k, n;
+	size_t m, i, j, k, n, lb;
 	listint_t *temp;
 
 	temp = list;
@@ -20,21 +20,23 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 		return (NULL);
 	m = sqrt(size);
 	i = 0;
+	j = 0;
 	while (temp->n < value && temp->next == NULL)
 	{
 		if (i + m >= size - 1)
 			i = size - 1;
 		else
 			i += m;
-		for (k = 0; k < m && temp->next != NULL; k++)
+		lb = j;
+		for (; j < i; j++)
 			temp = temp->next;
 		printf("Value checked array[%ld] = [%d]\n", i, temp->n);
 	}
-	printf("Value found between indexes [%ld] and [%ld]\n", i - k, i);
+	printf("Value found between indexes [%ld] and [%ld]\n", lb, i);
 	temp = list;
-	for (n = 0; n < i - k; n++)
+	for (n = 0; n < lb; n++)
 		temp = temp->next;
-	for (j = i - k; j < i + 1; j++)
+	for (j = lb; j < i + 1; j++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", j, temp->n);
 		if (temp->n == value)
