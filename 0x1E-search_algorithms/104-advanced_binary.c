c1bb622a0812a819f2723c379db25c76aa87b9fe
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-13 08:23:32 +0000

    advanced binary

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+/**
+ * advanced_binary - binary searching algorithm but with first occurence of
+ * the value implimentation
+ * @array: the index to the first array
+ * @size: the size of the array
+ * @value: the value to be searched
+ * Return: the index of the value if found, else return -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t l, r, m, i;
+
+	if (array == NULL)
+		return (-1);
+	l = 0;
+	r = size - 1;
+	while (l <= r)
+	{
+		printf("Searching in array: %d", array[l]);
+		for (i = l + 1; i <= r; i++)
+		{
+			printf(", %d", array[i]);
+		}
+		printf("\n");
+		m = (l + r) / 2;
+		if (array[m] > value)
+			r = m - 1;
+		else if (array[m] < value)
+			l = m + 1;
+		else
+			return (m);
+	}
+	return (-1);
+}
